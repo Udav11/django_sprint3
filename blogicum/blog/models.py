d04e3833680b21fdf491b49a7e4a8508575de2de
Udav <udavimpinf@gmail.com>
2025-01-24 14:38:56 +0300

    донастройка, все работает

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -4,16 +4,20 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Category(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(unique=True, verbose_name='Идентификатор', help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
+    slug = models.SlugField(unique=True, verbose_name='Идентификатор',
+                            help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
+
     def __str__(self):
-        return self.title 
+        return self.title
+
 
 class Location(PublishedModel):
     name = models.CharField(max_length=256, verbose_name='Название места')
@@ -21,23 +25,27 @@ class Location(PublishedModel):
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
+
     def __str__(self):
-        return self.title 
-    
+        return self.name
+
+
 class Post(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateTimeField(verbose_name='Дата и время публикации', help_text='Если установить дату и время в будущем — можно делать отложенные публикации.')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор публикации')
-    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Местоположение')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name='Категория')
+    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
+                                    help_text='Если установить дату и время в будущем — можно делать отложенные публикации.')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Автор публикации')
+    location = models.ForeignKey(
+        Location, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Местоположение')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, verbose_name='Категория')
 
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date', )
+
     def __str__(self):
-        return self.title 
-
-
-
-
+        return self.title
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,47 +2,43 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from blog.models import Post, Category
 from django.db.models import Q
+from django.db.models.functions import Now
 
 
 def index(request):
-    post_list = Post.objects.select_related('category').filter(
-        is_published = True,
-        category__is_published = True
+    post_list = Post.objects.select_related('author', 'category',
+                                            'location').filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lt=Now(),
     )[0:5]
-    context = {
-        'post_list': post_list,
-    }
-    return render(request, 'blog/index.html', context)
+    return render(request, 'blog/index.html', {'post_list': post_list})
 
 
 def post_detail(request, post_id):
     post_list = get_object_or_404(
-        Post.objects.filter(
-            
-        ), pk=post_id
+        Post.objects.select_related(
+            'author', 'category', 'location',
+        ).filter(pub_date__lt=Now(),), id=post_id
     )
-    context = {
-        'post_list': post_list
-    }
-    return render(request, 'blog/detail.html', context)
+    return render(request, 'blog/detail.html', {'post': post_list})
 
 
 def category_posts(request, category_slug):
     post_list = Post.objects.select_related('category').filter(
-        is_published = True,
-        category__slug = category_slug
+        is_published=True,
+        category__slug=category_slug
     )
     category = Category.objects.values().filter(
         slug=category_slug
     )
 
     if Category.objects.values('is_published').filter(
-        slug=category_slug)[0]['is_published'] is False:
+            slug=category_slug)[0]['is_published'] is False:
         raise Http404('Page not found')
-    
+
     context = {
         'category': category[0],
         'post_list': post_list
     }
     return render(request, 'blog/category.html', context)
-

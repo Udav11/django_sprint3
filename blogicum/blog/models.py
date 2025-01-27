36c4c5b2378309a56efc9d5a8fbbb93caa779eb7
Udav <udavimpinf@gmail.com>
2025-01-27 10:34:27 +0300

    Настройка админ-панели

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,8 +1,20 @@
 from django.db import models
-from core.models import PublishedModel
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
+class PublishedModel(models.Model):
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Добавлено')
+
+    class Meta:
+        abstract = True
+        ordering = ('created_at', )
 
 
 class Category(PublishedModel):
